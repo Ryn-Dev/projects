5c5ac8b05a5f76726fa63db37c74cffe635b4ea0
Febryan1453 <brayenfebryan@gmail.com>
2025-03-31 00:25:16 +0800

    Eid Mubarak 1446 H

---
+++ eid-1446/js/animate.js
@@ -0,0 +1,140 @@
+function animate(typewriter) {
+    sequence = [
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Assalamu ‘alaikum wa rahmatullahi wa barakatuh,\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Saya FEBRYAN mengucapkan...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Selamat Hari Raya Idul Fitri 1446 Hijriah.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Taqabbalallahu minna wa minkum.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Serta di hari kemenangan, hari yang suci dan penuh berkah ini,\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "jika saya pernah berbuat salah baik dari perilaku maupun...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "perkataan kepada kalian, maka ingatlah bahwa itu semua...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        // {
+        //     text: "MEMANG PANTAS UNTUK KALIAN BANG",
+        //     delayAfter: 1000
+        // },{
+        //     text: "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\badalah benar-benar kekhilafan saya.\n",
+        //     delayAfter: 500
+        // },
+        {
+            text: "adalah benar-benar kekhilafan saya.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Oleh karena itu, saya atas nama pribadi juga ingin meminta...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "maaf sebesar-besarnya untuk kekhilafan-kekhilafan...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "yang pernah dilakukan.\n",
+            delayAfter: 500
+        },
+        
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Minal Aidzin Wal Faidzin.Mohon maaf lahir dan batin 🙏😊🙏\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Wassalamu 'alaikum wa rahmatullahi wa barakatuh.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@febryan:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Salamaki Tapada Salama 😊\n",
+            delayAfter: 500
+        },
+    ];
+
+    typewriter.playSequence(sequence);
+}
